c4c378c5addad0a46a45c5739fb69116c8e23885
Todd Horst <toddmhorst@yahoo.com>
2024-12-23 09:19:52 -0500

    move to single executed query

--- Fridge/fridgeDigest.js
+++ Fridge/fridgeDigest.js
@@ -1,27 +1,6 @@
-/*
-    The first half deals with inserting the transaction into the fridge.
-        For the insert, we only insert the data from the first connector.
-        This works because on the global post processor, the source is always 
-        the first connector. And then we are saying that if you want to save
-        the destinations ack or message you need to use the response transformer
-        code template. If the message is coming from the response map, it will
-        only contain one connector, and it will be the one which pertains to that
-        instance of the message.
-
-
-    The second half deals with inserting the activity for each connector.
-        This loops over all the destinations, but if the destination is set to queue
-        its just an estimated sent date. To get the actual sent dateimttime you need
-        to use the respons transformer code template.
-
-
-*/
-
-
-
 var debug = false;
 
-// Convert the maps to one json object
+// Convert the maps to one JSON object
 var mapLoop = {
     "channel": msg.mapChannel,
     "response": msg.mapResponse,
@@ -30,14 +9,10 @@ var mapLoop = {
 };
 var maps = [];
 Object.keys(mapLoop).forEach(function (map) {
-
-    // Sometimes the map is empty
     var mapV = mapLoop[map];
     if (mapV == null) {
         return;
     }
-
-    // Create a standardized object for db field
     Object.keys(mapV).forEach(function (key) {
         var v = mapV[key];
         maps.push({
@@ -48,110 +23,40 @@ Object.keys(mapLoop).forEach(function (map) {
     });
 });
 
-
 // Build query to insert the message into the fridge
-var sql = "INSERT INTO fridge_message_history ( \
-		message_id,  \
-		channel_id, \
-		channel_name, \
-		connector_id,  \
-		connector_name,  \
-		send_state,  \
-		transmit_time, \
-		maps,  \
-		message,  \
-		response) \
-	VALUES (?,?,?,?,?,?,?,?,?,?)";
+var sql = "INSERT INTO fridge_message_history ( message_id,  channel_id, channel_name, connector_id,  connector_name,  send_state,  transmit_time, maps, message,  response) \
+VALUES ('" + msg.messageId + "', '" + msg.channelId + "', '" + msg.channelName + "', '" + msg.connectors[0].connectorId + "', '" + msg.connectors[0].connectorName + "', '" + msg.connectors[0].processingState + "', " + (msg.connectors[0].transmitDate / 1000) + ", '" + JSON.stringify(maps) + "', '" + msg.connectors[0].message + "', '" + msg.connectors[0].response + "');";
 
-// Get seconds from epoch, not milli
-var transmitDate = msg.connectors[0].transmitDate / 1000;
+msg.connectors.forEach(function (conn) {
+    var field_list = "channel_id, channel_name, connector_id, connector_name, updated";
+    var value_list = "'" + msg.channelId + "', '" + msg.channelName + "', '" + conn.connectorId + "', '" + conn.connectorName + "', NOW()";
+    var duplicate_list = "";
 
+    if (typeof conn.transmitDate != "undefined" && conn.transmitDate != 0) {
+        field_list += ", actual_transmit";
+        value_list += ", " + Math.floor(conn.transmitDate / 1000);
+        duplicate_list += " actual_transmit = VALUES(actual_transmit), ";
+    }
 
-// Build param list
-var param = [
-    msg.messageId,
-    msg.channelId,
-    msg.channelName,
-    msg.connectors[0].connectorId,
-    msg.connectors[0].connectorName,
-    msg.connectors[0].processingState,
-    transmitDate,
-    JSON.stringify(maps),
-    msg.connectors[0].message,
-    msg.connectors[0].response
-];
+    if (typeof conn.estimatedDate != "undefined" && conn.estimatedDate != 0) {
+        field_list += ", estimated_transmit";
+        value_list += ", " + Math.floor(conn.estimatedDate / 1000);
+        duplicate_list += " estimated_transmit = VALUES(estimated_transmit), ";
+    }
+
+    sql += "INSERT INTO last_activity (" + field_list + ") VALUES (" + value_list + ") ON DUPLICATE KEY UPDATE " + duplicate_list + " updated = NOW();";
+});
 
 if (debug) {
-    echo("Inserting into the message history.");
+    echo("Executing combined SQL:");
     echo(sql);
-    echo(param);
-    echo(db_DebugQuery(sql, param));
 }
 
-// Insert into message history
-var result = db_exec("dbMirthExt", sql, { query_parameters: param });
+// Execute the combined SQL
+var result = db_exec("dbMirthExt", sql);
 
 if (debug) {
     echo("Effected Rows:" + result);
 }
-
-
-
-msg.connectors.forEach(function (conn) {
-
-    // Build param list
-    var activityParam = [
-        msg.channelId,
-        msg.channelName,
-        conn.connectorId,
-        conn.connectorName,
-    ];
-
-    // Default values
-    var duplicate_list = "";
-    var field_list = "channel_id, channel_name, connector_id, connector_name, updated";
-    var value_list = "?,?,?,?,NOW()";
-
-    // If there is a transmitDate that means this connector doesn't use queueing so we will add it
-    // to the query
-    if (typeof conn.transmitDate != "undefined" && conn.transmitDate != 0) {
-        activityParam.push(Math.floor(conn.transmitDate / 1000));
-        duplicate_list += " actual_transmit = VALUES(actual_transmit), ";
-        field_list += ", actual_transmit";
-        value_list += ",?";
-    }
-
-    if (typeof conn.estimatedDate != "undefined" && conn.estimatedDate != 0) {
-        activityParam.push(Math.floor(conn.estimatedDate / 1000));
-        duplicate_list += " estimated_transmit = VALUES(estimated_transmit), ";
-        field_list += ", estimated_transmit";
-        value_list += ",?";
-    }
-
-    // Now insert into the last activity table
-    var activitySQL = "INSERT INTO last_activity \
-	                        ("+ field_list + ") \
-	                    VALUES \
-	                        ("+ value_list + ") \
-	                    ON DUPLICATE KEY UPDATE \
-	                        " + duplicate_list + " \
-	                        updated = NOW();";
-
-    if (debug) {
-        echo("About to insert connector activity.");
-        echo(activitySQL);
-        echo(activityParam);
-        echo(db_DebugQuery(activitySQL, activityParam));
-    }
-
-    // Do the insert for this connector
-    var result = db_exec("dbMirthExt", activitySQL, { query_parameters: activityParam });
-
-    if (debug) {
-        echo("con: " + conn.connectorId + " Effected Rows:" + result);
-    }
-});
-
-
 
 return true;
